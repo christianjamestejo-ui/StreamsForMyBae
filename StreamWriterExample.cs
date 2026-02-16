94042904dee5cf1acf6a8551fce95b31bb3aaf92
christianjamestejo-ui <christianjames.tejo@dbtc-cebu.edu.ph>
2026-02-16 16:23:21 +0800

    Add files via upload
    hey man assign for sir

---
+++ StreamWriterExample.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace StreamWriterExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input Name of Student:");
+            string StudentName = Console.ReadLine();
+
+            Console.WriteLine("Input ID Number:");
+            string StudentID = Console.ReadLine();
+
+            string path = "StreamWriterExample.txt";
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                writer.WriteLine($"Student Name: {StudentName}");
+                writer.WriteLine($"Student ID: {StudentID}");
+            }
+
+            try
+            {
+                string data = File.ReadAllText(path);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Error: File not found.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unexpected error: {ex.ToString()}");
+            }
+            finally
+            {
+                Console.WriteLine("I/O attempt complete");
+            }
+        }
+    }
+}
